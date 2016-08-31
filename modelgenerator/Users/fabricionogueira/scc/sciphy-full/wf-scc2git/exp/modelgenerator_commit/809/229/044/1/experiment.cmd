d9f436a1286273956c54d550c23cbe116c3b7a76
Fabricio <fabricio.nogueira@gmail.com>
2016-08-31 10:03:53 -0300

    modelgenerator_commit

---
+++ modelgenerator/Users/fabricionogueira/scc/sciphy-full/wf-scc2git/exp/modelgenerator_commit/809/229/044/1/experiment.cmd
@@ -0,0 +1,3 @@
+java -jar /Users/fabricionogueira/scc/scc2git.jar -lr /Users/fabricionogueira/scc/sciphy-full/wf-scc2git/localrepo -pwf /Users/fabricionogueira/scc/sciphy-full/wf-scc2git/SCC.xml -an modelgenerator -m modelgenerator_commit
+java -jar /Users/fabricionogueira/scc/sciphy-scc2git/bin/CreateERelation.jar "LOCAL_REPOSITORY;PATH_TO_WF_FILE;MESSAGE
+/Users/fabricionogueira/scc/sciphy-full/wf-scc2git/localrepo;/Users/fabricionogueira/scc/sciphy-full/wf-scc2git/SCC.xml;modelgenerator_commit"
