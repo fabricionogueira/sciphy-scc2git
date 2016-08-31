c1887d32ef6ff2fdab49ac048e86b1e673faa329
Fabricio <fabricio.nogueira@gmail.com>
2016-08-31 10:48:00 -0300

    raxml2_commit

---
+++ raxml2/Users/fabricionogueira/scc/sciphy-full/wf-scc2git/exp/raxml2_commit/103/641/164/0/experiment.cmd
@@ -0,0 +1,3 @@
+java -jar /Users/fabricionogueira/scc/scc2git.jar -lr /Users/fabricionogueira/scc/sciphy-full/wf-scc2git/localrepo -pwf /Users/fabricionogueira/scc/sciphy-full/wf-scc2git/SCC.xml -an raxml2 -m raxml2_commit
+java -jar /Users/fabricionogueira/scc/sciphy-scc2git/bin/CreateERelation.jar "LOCAL_REPOSITORY;PATH_TO_WF_FILE;MESSAGE
+/Users/fabricionogueira/scc/sciphy-full/wf-scc2git/localrepo;/Users/fabricionogueira/scc/sciphy-full/wf-scc2git/SCC.xml;raxml2_commit"
