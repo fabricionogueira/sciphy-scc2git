3178817567ea037eba43630a28985ffaef19996f
root <root@ip-172-31-2-33.ec2.internal>
2016-11-01 12:49:08 -0400

    raxml3_commit

---
+++ raxml3/mnt/scc2git/wf-scc2git/exp/raxml3_commit/916/322/503/0/experiment.cmd
@@ -0,0 +1,3 @@
+/opt/jdk1.8.0_101/bin/java -jar /mnt/scc2git/wf-scc2git/scc2git.jar -lr /mnt/scc2git/wf-scc2git/localrepo -pwf /mnt/scc2git/wf-scc2git/SCC.xml -an raxml3 -m raxml3_commit
+/opt/jdk1.8.0_101/bin/java -jar /mnt/scc2git/wf-scc2git/bin/CreateERelation.jar "LOCAL_REPOSITORY;PATH_TO_WF_FILE;MESSAGE
+/mnt/scc2git/wf-scc2git/localrepo;/mnt/scc2git/wf-scc2git/SCC.xml;raxml3_commit"
