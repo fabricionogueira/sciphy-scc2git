4226876931198f5c9ca7645840619bd971187e53
root <root@ip-172-31-2-33.ec2.internal>
2016-11-06 20:02:10 -0500

    readseq_commit

---
+++ readseq/141/759/522/0/experiment.cmd
@@ -0,0 +1,4 @@
+#!/bin/bash
+/opt/jdk1.8.0_101/bin/java -jar /root/wf-scc2git/bin/readseq.jar -all -f=12 /root/wf-scc2git/exp/readseq/141/759/522/0/i.mafft -o /root/wf-scc2git/exp/readseq/141/759/522/0/i.phylip
+
+python /root/wf-scc2git/bin/extractor.py `basename /root/wf-scc2git/exp/readseq/141/759/522/0/i` PA_Readseq ERelation.txt
