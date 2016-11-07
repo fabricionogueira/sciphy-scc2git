37055e77b7151c6117f958d15c5e6cf63b1c55bc
root <root@ip-172-31-2-33.ec2.internal>
2016-11-06 22:06:49 -0500

    raxml2_commit

---
+++ raxml2/123/729/683/0/experiment.cmd
@@ -0,0 +1,5 @@
+#!/bin/bash
+# python /root/wf-scc2git/../bin/execute_raxml.py . /root/wf-scc2git/exp/raxml2/123/729/683/0/i.phylip /root/wf-scc2git/exp/raxml2/123/729/683/0/i.mg.modelFromMG.txt 100 4 2
+python /root/wf-scc2git/bin/execute_raxml.py . /root/wf-scc2git/exp/raxml2/123/729/683/0/i.phylip /root/wf-scc2git/exp/raxml2/123/729/683/0/i.mg.modelFromMG.txt 4 4 2
+
+python /root/wf-scc2git/bin/extractor.py `basename /root/wf-scc2git/exp/raxml2/123/729/683/0/i` PA_Raxml2 ERelation.txt
