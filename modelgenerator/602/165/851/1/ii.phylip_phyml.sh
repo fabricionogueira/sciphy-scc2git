4b2aebc3b03a2d9632e9bf3be972fb05a7791926
root <root@ip-172-31-13-245.ec2.internal>
2016-11-21 19:20:31 -0500

    modelgenerator_commit

---
+++ modelgenerator/602/165/851/1/ii.phylip_phyml.sh
@@ -0,0 +1,1 @@
+phyml /mnt/scc2git/exp/modelgenerator/602/165/851/1/ii.phylip 1 i 1 0 WAG 0.0 1 1.0 BIONJ y y
---
+++ modelgenerator/602/165/851/1/ii.phylip_treePuzzle.sh
@@ -0,0 +1,9 @@
+puzzle /mnt/scc2git/exp/modelgenerator/602/165/851/1/ii.phylip <<EOF
+m
+m
+m
+m
+m
+m
+y
+EOF
