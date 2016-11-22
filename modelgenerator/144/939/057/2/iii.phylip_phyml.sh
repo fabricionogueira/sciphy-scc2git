e20dad97932e95337aaa5e5e36b071c065527bad
root <root@ip-172-31-13-245.ec2.internal>
2016-11-21 19:02:23 -0500

    modelgenerator_commit

---
+++ modelgenerator/144/939/057/2/iii.phylip_phyml.sh
@@ -0,0 +1,1 @@
+phyml /mnt/scc2git/exp/modelgenerator/144/939/057/2/iii.phylip 1 i 1 0 WAG 0.0 1 1.0 BIONJ y y
---
+++ modelgenerator/144/939/057/2/iii.phylip_treePuzzle.sh
@@ -0,0 +1,9 @@
+puzzle /mnt/scc2git/exp/modelgenerator/144/939/057/2/iii.phylip <<EOF
+m
+m
+m
+m
+m
+m
+y
+EOF
